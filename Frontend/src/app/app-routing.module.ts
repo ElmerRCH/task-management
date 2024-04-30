c81122e5947bf91d130d5017dc7a17f42c94ecd1
Elmer Camacho <renehiguera15@gmail.com>
2024-04-30 15:56:28 -0700

    add-login-register

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterUserComponent } from './register-user/register-user.component';
+import { LoginUserComponent } from './login-user/login-user.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginUserComponent },
+  { path: 'register-users', component: RegisterUserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # task management
 # Administrador de tareas
+# https://aprendepythonhoy.com/proyectos-practicos-para-mejorar-tu-portafolio-en-python/
