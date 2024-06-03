d8682997b3eb66a13436000ace82fe7d62a7dcc8
Elmer Camacho <renehiguera15@gmail.com>
2024-06-02 21:42:17 -0700

    bd name

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }"""
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/postgres',
         conn_max_age=600
     )
 }
