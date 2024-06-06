5ae23af7187c0ae08a15f6ad45c63674b40056e9
Elmer Camacho <renehiguera15@gmail.com>
2024-06-06 09:26:53 -0700

    fix sh

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Ejecutar migraciones
+echo "Applying database migrations........."
 python manage.py migrate
 
 # Iniciar Gunicorn
+echo "Starting Gunicorn server..."
 exec gunicorn Backend.wsgi:application --bind 0.0.0.0:8000
